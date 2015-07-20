5c0d98cbea54908815f367c74fef033f1012c9cd
CodeKirin <tony_yu_1990@hotmail.com>
2015-07-19 21:47:12 -0400

    the fks
    indeed

--- js/board.js
+++ js/board.js
@@ -10,10 +10,10 @@ function Board(stage, windowWidth, windowHeight) {
   /***************************************** Board Setup *****************************************/
                                              /* Tiles */
   // Corner tiles
-  var corner1Start = PIXI.Texture.fromImage("images/jail.png");
+  var corner1Start = PIXI.Texture.fromImage("images/gogogo.png");
   var corner2Jail = PIXI.Texture.fromImage("images/jail.png");
-  var corner3Parking = PIXI.Texture.fromImage("images/jail.png");
-  var corner4GoJail = PIXI.Texture.fromImage("images/jail.png");
+  var corner3Parking = PIXI.Texture.fromImage("images/freeparking.png");
+  var corner4GoJail = PIXI.Texture.fromImage("images/gotojail.png");
 
   var corner2 = new PIXI.Sprite(corner2Jail);
   corner2.position.x = 125;
@@ -26,30 +26,56 @@ function Board(stage, windowWidth, windowHeight) {
   this.board.addChild(corner3);
 
   // Bottom tiles
-
-
+  var tile1Geodude = PIXI.Texture.fromImage("images/geodude.png");
+  var tile2AshBot = PIXI.Texture.fromImage("images/ash(bot).png");
   var tile3Onix = PIXI.Texture.fromImage("images/onix.png");
   var tile4Gary = PIXI.Texture.fromImage("images/gary.png");
-
+  var tile5Pokeball = PIXI.Texture.fromImage("images/pokeball.png");
   var tile6Staryu = PIXI.Texture.fromImage("images/staryu.png");
-  var tile7Oak = PIXI.Texture.fromImage("images/oak.png");
+  var tile7OakBot = PIXI.Texture.fromImage("images/oak(bot).png");
   var tile8Starmie = PIXI.Texture.fromImage("images/starmie.png");
   var tile9Horsea = PIXI.Texture.fromImage("images/horsea.png");
-  var bottomTiles = [corner1Start, tile3Onix, tile3Onix, tile3Onix, tile3Onix, tile4Gary,
-  tile6Staryu, tile7Oak, tile8Starmie, tile9Horsea];
+  var bottomTiles = [corner1Start, tile1Geodude, tile2AshBot, tile3Onix, tile4Gary, tile5Pokeball,
+  tile6Staryu, tile7OakBot, tile8Starmie, tile9Horsea];
 
   // Left tiles
-  var tile2Voltorb = PIXI.Texture.fromImage("images/voltorb.png");
-  var leftTiles = [tile2Voltorb, tile2Voltorb, tile2Voltorb, tile2Voltorb, tile2Voltorb, tile2Voltorb,
-  tile2Voltorb, tile2Voltorb, tile2Voltorb];
+  var tile1Voltorb = PIXI.Texture.fromImage("images/voltorb.png");
+  var tile2Zapdos = PIXI.Texture.fromImage("images/zapdos.png");
+  var tile3Electabuzz = PIXI.Texture.fromImage("images/electabuzz.png");
+  var tile4Raichu = PIXI.Texture.fromImage("images/raichu.png");
+  var tile5Greatball = PIXI.Texture.fromImage("images/greatball.png");
+  var tile6Victreebel = PIXI.Texture.fromImage("images/victreebel.png");
+  var tile7AshLeft = PIXI.Texture.fromImage("images/ash(left).png");
+  var tile8Tangela = PIXI.Texture.fromImage("images/tangela.png");
+  var tile9Vileplume = PIXI.Texture.fromImage("images/vileplume.png");
+  var leftTiles = [tile1Voltorb, tile2Zapdos, tile3Electabuzz, tile4Raichu, tile5Greatball, 
+  tile6Victreebel, tile7AshLeft, tile8Tangela, tile9Vileplume];
 
   // Top tiles
-  var topTiles = [tile3Onix, tile3Onix, tile3Onix, tile3Onix, tile4Gary,
-  tile6Staryu, tile7Oak, tile8Starmie, tile9Horsea, corner4GoJail];
+  var tile1Kadabra = PIXI.Texture.fromImage("images/kadabra.png");
+  var tile2OakTop = PIXI.Texture.fromImage("images/oak(top).png");
+  var tile3MrMime = PIXI.Texture.fromImage("images/mrmime.png");
+  var tile4Venomoth = PIXI.Texture.fromImage("images/venomoth.png");
+  var tile5Ultraball = PIXI.Texture.fromImage("images/ultraball.png");
+  var tile6Koffing = PIXI.Texture.fromImage("images/koffing.png");
+  var tile7Muk = PIXI.Texture.fromImage("images/muk.png");
+  var tile8Articuno = PIXI.Texture.fromImage("images/articuno.png");
+  var tile9Weezing = PIXI.Texture.fromImage("images/weezing.png");
+  var topTiles = [tile1Kadabra, tile2OakTop, tile3MrMime, tile4Venomoth, tile5Ultraball,
+  tile6Koffing, tile7Muk, tile8Articuno, tile9Weezing, corner4GoJail];
 
   // Right tiles
-  var rightTiles = [tile2Voltorb, tile2Voltorb, tile2Voltorb, tile2Voltorb, tile2Voltorb, tile2Voltorb,
-  tile2Voltorb, tile2Voltorb, tile2Voltorb];
+  var tile1Growlithe = PIXI.Texture.fromImage("images/growlithe.png");
+  var tile2Ponyta = PIXI.Texture.fromImage("images/ponyta.png");
+  var tile3AshRight = PIXI.Texture.fromImage("images/ash(right).png");
+  var tile4Rapidash = PIXI.Texture.fromImage("images/rapidash.png");
+  var tile5Masterball = PIXI.Texture.fromImage("images/masterball.png");
+  var tile6OakRight = PIXI.Texture.fromImage("images/oak(right).png");
+  var tile7Nidoqueen = PIXI.Texture.fromImage("images/nidoqueen.png");
+  var tile8TeamRocket = PIXI.Texture.fromImage("images/teamrocket.png");
+  var tile9Nidoking = PIXI.Texture.fromImage("images/nidoking.png");
+  var rightTiles = [tile1Growlithe, tile2Ponyta, tile3AshRight, tile4Rapidash, tile5Masterball,
+  tile6OakRight, tile7Nidoqueen, tile8TeamRocket, tile9Nidoking];
 
 
                         /* Sides */
--- js/game.js
+++ js/game.js
@@ -1,6 +1,5 @@
 function game()
 {
-
     // Dice roller
     var dice = D6;
     dice.setBaseUrl("diceRoller/");
@@ -9,7 +8,6 @@ function game()
     /******************************************* Functions *******************************************/
     // Setting hidden result label
     function attack(result) {
-
       var allBoardTiles = gameBoard.getAllBoardTiles();
 
       // Displaying the result of the input for testing
@@ -17,10 +15,7 @@ function game()
          var newIndex = currentIndex == -1  ? newIndex : currentIndex + result;
          newIndex = newIndex >= allBoardTiles.length - 1 ? newIndex - allBoardTiles.length + 1  : newIndex;
         player.moveTo(allBoardTiles[newIndex]);
-
-
     }
-
 
     /****************************************** Display ***********************************************/
     // Width and height of window for the client
@@ -37,51 +32,33 @@ function game()
     var gameBoard = new Board(stage, windowWidth, windowHeight);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     // create an array of textures from an image path
     var frames = [];
 
-  for (var i = 0; i < 4; i++) {
-      // magically works since the spritesheet was loaded with the pixi loader
-      frames.push(PIXI.Texture.fromFrame(i));
-  }
-  // create a MovieClip (brings back memories from the days of Flash, right ?)
-  movie = new PIXI.extras.MovieClip(frames);
-  /*
-   * A MovieClip inherits all the properties of a PIXI sprite
-   * so you can change its position, its anchor, mask it, etc
-   */
-  movie.position.set(300);
+    for (var i = 0; i < 4; i++) {
+		// magically works since the spritesheet was loaded with the pixi loader
+		frames.push(PIXI.Texture.fromFrame(i));
+	}
+	// create a MovieClip (brings back memories from the days of Flash, right ?)
+	movie = new PIXI.extras.MovieClip(frames);
+	/*
+	 * A MovieClip inherits all the properties of a PIXI sprite
+	 * so you can change its position, its anchor, mask it, etc
+	 */
+	movie.position.set(300);
+	movie.animationSpeed = 0.1;
+    movie.play();
 
-  movie.animationSpeed = 0.1;
-
-  movie.play();
-
-    var player = new PlayerFromMovie(movie);
-    player.addToStage(stage);
-    player.moveTo(gameBoard.getAllBoardTiles()[0]);
-
+	var player = new PlayerFromMovie(movie);
+	player.addToStage(stage);
+	player.moveTo(gameBoard.getAllBoardTiles()[0]);
 
     // start animating
-    animate();
-    function animate(time) {
-      requestAnimationFrame(animate);
-      // render the container
-      renderer.render(stage);
-      TWEEN.update(time);
+	animate();
+	function animate(time) {
+	    requestAnimationFrame(animate);
+	    // render the container
+		renderer.render(stage);
+		TWEEN.update(time);
     }
-
-  }
+}
